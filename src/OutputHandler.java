6662dc560eaa048aa7c2224767900604d33f6c36
Benix1239 <beni.beke@gmail.com>
2025-04-25 11:44:45 +0200

    osszehasonlitas

--- src/OutputHandler.java
+++ src/OutputHandler.java
@@ -1,9 +1,11 @@
 
+import java.io.File;
 import java.io.PrintStream;
 
 public class OutputHandler 
 {
     private  static PrintStream kimenet = new PrintStream(System.out);
+    private static File forras = null;
 
     public OutputHandler() 
     {
@@ -19,6 +21,16 @@ public class OutputHandler
     public static PrintStream getKimenet()
     {
         return kimenet;
+    }
+    
+    public static File getForras()
+    {
+        return forras;
+    }
+
+    public static void setForras(File f)
+    {
+        forras = f;
     }
 
     public static void Close()
