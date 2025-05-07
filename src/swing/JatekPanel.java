21c71b697e4bc6144e666c23f1fc5f2a3e5fd61d
LenardAbel <lenardabel2004@gmail.com>
2025-05-07 21:31:40 +0200

    dwasdwaaadddssseee

--- src/swing/JatekPanel.java
+++ src/swing/JatekPanel.java
@@ -13,11 +13,8 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.Collections;
-<<<<<<< HEAD
 
 import javax.swing.AbstractAction;
-=======
->>>>>>> 63e35e424f196dcf03f5195273bdd65a61a4ad4b
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
